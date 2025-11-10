f1bc0ad88994c0e6c02db9fc06b815c77636d80c
Hidalgo <andrei.kyle.hidalgo@accenture.com>
2025-11-10 22:49:55 +0800

    Initial commit

---
+++ api/noAuthentication.js
@@ -0,0 +1,10 @@
+// api/noAuthentication.js
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello from Express on Vercel!" });
+});
+
+export default app;
